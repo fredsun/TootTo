efa4cc8dbdb210861974b97e63674254613ec39b
sunxl <fredsun1206@gmail.com>
2018-01-04 11:32:50 +0800

    feat: 给preference添加监听

--- app/src/main/java/org/tootto/activity/SettingActivity.java
+++ app/src/main/java/org/tootto/activity/SettingActivity.java
@@ -1,10 +1,20 @@
 package org.tootto.activity;
 
+import android.content.SharedPreferences;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatDelegate;
+import android.text.TextUtils;
+import android.util.Log;
 
 import org.tootto.R;
 
@@ -15,6 +25,82 @@ import java.util.List;
  */
 
 public class SettingActivity extends PreferenceActivity {
+    final String TAG = "SettingActivity";
+    //原registerOnSharedPreferenceChangeListener是弱引用注册，会被回收，所以全局变量
+    static Preference.OnPreferenceChangeListener bindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener(){
+
+        //ListPreference 和 RingtonePreference 的 summary 过多，分开处理
+        //stolen from AndroidStudio Sample Project
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            if (preference instanceof ListPreference){
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                // Set the summary to reflect the new value.
+                preference.setSummary(
+                        index >= 0
+                                ? listPreference.getEntries()[index]
+                                : null);
+            }else if (preference instanceof RingtonePreference){
+                // For ringtone preferences, look up the correct display value
+                // using RingtoneManager.
+                if (TextUtils.isEmpty(stringValue)) {
+                    // Empty values correspond to 'silent' (no ringtone).
+                    preference.setSummary(R.string.pref_ringtone_silent);
+
+                } else {
+                    Ringtone ringtone = RingtoneManager.getRingtone(
+                            preference.getContext(), Uri.parse(stringValue));
+
+                    if (ringtone == null) {
+                        // Clear the summary if there was a lookup error.
+                        preference.setSummary(null);
+                    } else {
+                        // Set the summary to reflect the new ringtone display
+                        // name.
+                        String name = ringtone.getTitle(preference.getContext());
+                        preference.setSummary(name);
+                    }
+                }
+            }else {
+                // For all other preferences, set the summary to the value's
+                // simple string representation.
+                preference.setSummary(stringValue);
+            }
+
+            return true;
+        }
+    };
+
+    private static void bindPreferenceSummaryToValue(Preference preference, Boolean isBoolean){
+        //绑定listener
+        preference.setOnPreferenceChangeListener(bindPreferenceSummaryToValueListener);
+        if (isBoolean){
+            bindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                    PreferenceManager
+                            .getDefaultSharedPreferences(preference.getContext())
+                            .getBoolean(preference.getKey(),false));
+        }else {
+            bindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                    PreferenceManager
+                            .getDefaultSharedPreferences(preference.getContext())
+                            .getString(preference.getKey(),""));
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Log.i(TAG, "SPtest_key"+defaultSharedPreferences.getString("test_key",""));
+        Log.i(TAG, "SPsave_data"+defaultSharedPreferences.getBoolean("save_data",false));
+
+    }
 
     @Override
     public void onBuildHeaders(List<Header> target) {
@@ -35,6 +121,8 @@ public class SettingActivity extends PreferenceActivity {
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_network);
+            bindPreferenceSummaryToValue(findPreference("test_key"), false);
+            bindPreferenceSummaryToValue(findPreference("save_data"), true);
         }
     }
 
