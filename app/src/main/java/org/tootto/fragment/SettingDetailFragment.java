d89c7b51fe3024955673f53b87464e9085a39d5f
sunxl <fredsun1206@gmail.com>
2018-01-28 22:56:05 +0800

    docs: 清空不需要的饮用

--- app/src/main/java/org/tootto/fragment/SettingDetailFragment.java
+++ app/src/main/java/org/tootto/fragment/SettingDetailFragment.java
@@ -2,17 +2,14 @@ package org.tootto.fragment;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.util.Log;
 import android.view.View;
 
-import org.tootto.activity.SetActivity;
 import org.tootto.util.Utils;
 
 /**
