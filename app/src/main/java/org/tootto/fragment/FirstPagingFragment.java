fbffe2b22413e5dafe5b12879018b8692a70d59a
sunxl <fredsun1206@gmail.com>
2018-04-03 09:41:56 +0800

    feat: 初步完成 timelinefragment 的加载

--- app/src/main/java/org/tootto/fragment/FirstPagingFragment.java
+++ app/src/main/java/org/tootto/fragment/FirstPagingFragment.java
@@ -1,5 +1,6 @@
 package org.tootto.fragment;
 
+import android.arch.core.util.Function;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
 
 import org.tootto.R;
 import org.tootto.adapter.FirstFragmentAdapter;
+import org.tootto.adapter.TimeLineAdapter;
 import org.tootto.anim.TitleBehaviorAnim;
 import org.tootto.backinterface.BackHandlerHelper;
 import org.tootto.backinterface.FragmentBackHandler;
@@ -23,6 +25,13 @@ import org.tootto.ui.view.observablescrollview.FrameInterceptLayout;
 import org.tootto.ui.view.observablescrollview.ObservableRecyclerView;
 import org.tootto.ui.view.observablescrollview.ObservableScrollViewCallbacks;
 import org.tootto.ui.view.observablescrollview.ScrollState;
+import org.tootto.util.CollectionUtil;
+import org.tootto.util.Either;
+import org.tootto.util.HttpHeaderLink;
+import org.tootto.util.ListUtils;
+import org.tootto.util.PairedList;
+import org.tootto.util.ViewDataUtils;
+import org.tootto.viewdata.StatusViewData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,7 +49,6 @@ public class FirstPagingFragment extends BaseFragment implements ObservableScrol
     final String TAG = "FirstPagingFragment";
     ObservableRecyclerView recyclerFirstFragment;
     ArrayList<String> mList = new ArrayList<>();
-    FirstFragmentAdapter mAdapter;
     LinearLayoutManager mLinearLayoutManager;
 
     TitleBehaviorAnim mTitleAnim;
@@ -51,6 +59,37 @@ public class FirstPagingFragment extends BaseFragment implements ObservableScrol
     private int mSlop;
     FrameInterceptLayout intercept_layout;
     private OkHttpClient.Builder builder;
+    private TimeLineAdapter timeLineAdapter;
+    private static final int LOAD_AT_ONCE = 30;
+    @Nullable
+    private String bottomId;
+    @Nullable
+    private String topId;
+
+    private boolean alwaysShowSensitiveMedia;
+    private PairedList<Either<Placeholder, Status>, StatusViewData> statuses =
+            new PairedList<>(new Function<Either<Placeholder, Status>, StatusViewData>() {
+                @Override
+                public StatusViewData apply(Either<Placeholder, Status> input) {
+                    Status status = input.getAsRightOrNull();
+                    if (status != null) {
+                        return ViewDataUtils.statusToViewData(status, alwaysShowSensitiveMedia);
+                    } else {
+                        return new StatusViewData.Placeholder(false);
+                    }
+                }
+            });
+
+    private static final class Placeholder {
+        private final static Placeholder INSTANCE = new Placeholder();
+
+        public static Placeholder getInstance() {
+            return INSTANCE;
+        }
+
+        private Placeholder() {
+        }
+    }
 
     @Nullable
     @Override
@@ -60,7 +99,7 @@ public class FirstPagingFragment extends BaseFragment implements ObservableScrol
             mList.add("i");
         }
 
-        mAdapter = new FirstFragmentAdapter(mList);
+        timeLineAdapter = new TimeLineAdapter();
         mLinearLayoutManager = new LinearLayoutManager(getContext());
         intercept_layout = view.findViewById(R.id.intercept_layout);
         intercept_layout.setDispatchTouchListener(this);
@@ -70,7 +109,7 @@ public class FirstPagingFragment extends BaseFragment implements ObservableScrol
 //        if (parentFragment instanceof ObservableScrollViewCallbacks) {
 //            recyclerFirstFragment.setScrollViewCallbacks((ObservableScrollViewCallbacks) parentFragment);
 //        }
-        recyclerFirstFragment.setAdapter(mAdapter);
+        recyclerFirstFragment.setAdapter(timeLineAdapter);
         recyclerFirstFragment.setLayoutManager(mLinearLayoutManager);
         recyclerFirstFragment.setScrollViewCallbacks(this);
         recyclerFirstFragment.addOnItemTouchListener(new RecyclerViewClickListener(getContext(), recyclerFirstFragment, new RecyclerViewClickListener.OnItemClickListener() {
@@ -117,7 +156,68 @@ public class FirstPagingFragment extends BaseFragment implements ObservableScrol
         listCall.enqueue(callback);
     }
 
-    private void onFetchTimeLineSuccess(List<Status> body, String linkHeader) {
+    /**
+     * 解析timeline/home的返回值
+     * 请求头中存放有两组值 next 和 max_id(较小值), prev 和 since_id(较大值)
+     * 第一次时直接放进adapter, 第二次时更新本地记录的timeline最大最小id值
+     * max_id = fromId, since_id = upToId .
+     * @param statuses
+     * @param linkHeader
+     */
+    private void onFetchTimeLineSuccess(List<Status> statuses, String linkHeader) {
+        List<HttpHeaderLink> links = HttpHeaderLink.parse(linkHeader);
+        boolean fullFetch = statuses.size() >= LOAD_AT_ONCE;
+        HttpHeaderLink next = HttpHeaderLink.findByRelationType(links, "next");
+        String fromId = null;
+        if (next != null){
+            fromId = next.uri.getQueryParameter("max_id");
+        }
+        //区分下拉刷新还是第一次
+        if (timeLineAdapter.getItemCount() > 1){
+            addItems(statuses, fromId);
+        }else {
+            HttpHeaderLink prev = HttpHeaderLink.findByRelationType(links, "prev");
+            String upToId = null;
+            if (prev != null){
+                upToId = prev.uri.getQueryParameter("since_id");
+                updateStatus(statuses, fromId, upToId, fullFetch);
+            }
+        }
+    }
+
+    private void updateStatus(List<Status> newStatuses, String fromId, String upToId, boolean fullFetch) {
+        if (ListUtils.isEmpty(newStatuses)){
+            return;
+        }
+        if (fromId != null){
+            bottomId = fromId;
+        }
+        if (upToId != null){
+            topId = upToId;
+        }
+        List<Either<Placeholder, Status>> liftedNew = listStatusList(newStatuses);
+        if (statuses.isEmpty()){
+            statuses.addAll(liftedNew);
+        }else{
+            Either<Placeholder, Status> lastOfNew = liftedNew.get(newStatuses.size() - 1);
+            int index = liftedNew.indexOf(lastOfNew);
+            for(int i=0; i<index; i++){
+                statuses.remove(0);
+            }
+            int newIndex = liftedNew.indexOf(statuses.get(0));
+            if (newIndex == -1){
+                if (index == -1 && fullFetch){
+                    liftedNew.add(Either.left(Placeholder.getInstance()));
+                }
+                statuses.addAll(0, liftedNew);
+            }else {
+                statuses.addAll(0, liftedNew.subList(0, newIndex));
+            }
+        }
+        timeLineAdapter.update(statuses.getPairedCopy());
+    }
+
+    private void addItems(List<Status> statusList, String fromId) {
 
     }
 
@@ -196,6 +296,13 @@ public class FirstPagingFragment extends BaseFragment implements ObservableScrol
             mTitleAnim.show();
             isTitleHide = false;
         }
+    }
+
+    private final Function<Status, Either<Placeholder, Status>> statusLifter =
+            Either::right;
+
+    private List<Either<Placeholder, Status>> listStatusList(List<Status> list) {
+        return CollectionUtil.map(list, statusLifter);
     }
 
     public void request() {
