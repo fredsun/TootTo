2beba54367ed6d08b8c738433a6a205a707c9bfd
sunxl <fredsun1206@gmail.com>
2018-04-03 09:39:28 +0800

    feat: timeline 上的 status

---
+++ app/src/main/java/org/tootto/adapter/StatusViewHolder.java
@@ -0,0 +1,30 @@
+package org.tootto.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import org.tootto.R;
+import org.tootto.viewdata.StatusViewData;
+
+/**
+ * Created by fred on 2018/3/31.
+ */
+
+class StatusViewHolder extends AbstractStatusBaseViewHolder{
+    TextView statusReblogerName;
+
+    public StatusViewHolder(View itemView) {
+        super(itemView);
+        statusReblogerName = itemView.findViewById(R.id.tv_status_rebloger_name);
+    }
+
+    private void setReblogerName(String name){
+        statusReblogerName.setText(name);
+    }
+
+    public void setUpWithStatus(StatusViewData.Concrete statusViewData) {
+        super.setUpWithStatus(statusViewData);
+
+    }
+}
