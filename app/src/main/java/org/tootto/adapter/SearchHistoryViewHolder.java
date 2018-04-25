1076758e577206a701c97103b4c3d4df21e8dd90
sunxl <fredsun1206@gmail.com>
2018-04-25 19:12:50 +0800

    feat: room设置schemas后生成的json

---
+++ app/src/main/java/org/tootto/adapter/SearchHistoryViewHolder.java
@@ -0,0 +1,42 @@
+package org.tootto.adapter;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.tootto.R;
+import org.tootto.dao.SearchHistory;
+import org.tootto.listener.SearchActionListener;
+
+import java.util.ArrayList;
+
+public class SearchHistoryViewHolder extends ViewHolder {
+    ConstraintLayout layout_search_history;
+    TextView tv_searchHistory;
+
+    public SearchHistoryViewHolder(View itemView) {
+        super(itemView);
+        layout_search_history = itemView.findViewById(R.id.layout_searchHistory);
+        tv_searchHistory = itemView.findViewById(R.id.tv_searchHistory);
+    }
+
+    public void setTextSearchHistory(String searchHistory, SearchActionListener listener) {
+        tv_searchHistory.setText(searchHistory);
+        View.OnClickListener viewClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onItemClick(getAdapterPosition());
+            }
+        };
+
+        tv_searchHistory.setOnClickListener(viewClickListener);
+        layout_search_history.setOnClickListener(viewClickListener);
+    }
+
+    public void setUpWithStatus(SearchHistory searchHistoryData, SearchActionListener listener) {
+        setTextSearchHistory(searchHistoryData.getHistorytext(), listener);
+    }
+}
