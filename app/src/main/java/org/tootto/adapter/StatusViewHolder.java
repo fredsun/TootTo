c627c6550315b5b370d2b989a4a9a9207ff20bae
sunxl <fredsun1206@gmail.com>
2018-04-26 20:46:52 +0800

    feat: timeline 添加 转发的中文text和转发png, 通过tintdrawable染色

--- app/src/main/java/org/tootto/adapter/StatusViewHolder.java
+++ app/src/main/java/org/tootto/adapter/StatusViewHolder.java
@@ -1,7 +1,11 @@
 package org.tootto.adapter;
 
-import android.support.v7.widget.RecyclerView;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.tootto.R;
@@ -14,18 +18,41 @@ import org.tootto.viewdata.StatusViewData;
 
 class StatusViewHolder extends AbstractStatusBaseViewHolder{
     TextView statusReblogerName;
+    ImageView ivReblogIcon;
 
     public StatusViewHolder(View itemView) {
         super(itemView);
         statusReblogerName = itemView.findViewById(R.id.tv_status_rebloger_name);
+        ivReblogIcon = itemView.findViewById(R.id.ivReblogIcon);
     }
 
     private void setReblogerName(String name){
-        statusReblogerName.setText(name);
+
+        Context context = statusReblogerName.getContext();
+        String format = context.getString(R.string.status_boosted_format);
+        String boostedText = String.format(format, name);
+        statusReblogerName.setText(boostedText);
+        Drawable originalDrawable = ContextCompat.getDrawable(context, R.drawable.ic_action_reblog);
+        Drawable.ConstantState state = originalDrawable.getConstantState();
+        Drawable tintDrawable = DrawableCompat.wrap(state == null ? originalDrawable : state.newDrawable()).mutate();
+        DrawableCompat.setTint(tintDrawable, context.getResources().getColor(R.color.colorIconUnselected));
+        ivReblogIcon.setImageDrawable(tintDrawable);
+//        statusReblogerName.setCompoundDrawablesWithIntrinsicBounds(tintDrawable, null, null, null);
+    }
+    private void hideReblogerName(){
+        if (statusReblogerName != null){
+            statusReblogerName.setVisibility(View.GONE);
+        }
     }
 
     @Override
     public void setUpWithStatus(StatusViewData.Concrete statusViewData, StatusActionListener listener, boolean mediaPreviewEnabled) {
         super.setUpWithStatus(statusViewData, listener, mediaPreviewEnabled);
+        String rebloggedByUsername = statusViewData.getRebloggedByUsername();
+        if (rebloggedByUsername == null){
+            hideReblogerName();
+        }else {
+            setReblogerName(rebloggedByUsername);
+        }
     }
 }
