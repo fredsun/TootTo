d04c532d258d4631304d539e28b7bab60cb0ace0
sunxl <fredsun1206@gmail.com>
2018-04-28 20:55:42 +0800

    feat: 图片详细页activity+viewpager+fragment.photoView. 保存图片添加写内存的权限并实时请求

---
+++ app/src/main/java/org/tootto/adapter/ImagePagerAdapter.java
@@ -0,0 +1,40 @@
+package org.tootto.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+import org.tootto.fragment.ViewMediaFragment;
+
+import java.util.Locale;
+
+public class ImagePagerAdapter extends FragmentPagerAdapter {
+    private String[] urls;
+    private int initialPosition;
+
+    public ImagePagerAdapter(FragmentManager fragmentManager, String[] urls, int initialPosition) {
+        super(fragmentManager);
+        this.urls = urls;
+        this.initialPosition = initialPosition;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position >= 0 && position < urls.length) {
+            return ViewMediaFragment.newInstance(urls[position], position == initialPosition);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return urls.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return String.format(Locale.getDefault(), "%d/%d", position + 1, urls.length);
+    }
+}
